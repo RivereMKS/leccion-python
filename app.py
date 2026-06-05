cb43acdcf16630701aa7ca19c1f9e4591a680447
RivereMKS <288481799+RivereMKS@users.noreply.github.com>
2026-06-04 21:13:59 -0600

    Agregando slider y boton de ejecutar

--- app.py
+++ app.py
@@ -2,4 +2,14 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
+# Widget 1: El control deslizante para elegir los intentos
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+
+# Widget 2: El botón para iniciar el experimento
+start_button = st.button('Ejecutar')
+
+# Acción que ocurre solo cuando se presiona el botón
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
 st.write('Esta aplicación aún no es funcional. En construcción.')
